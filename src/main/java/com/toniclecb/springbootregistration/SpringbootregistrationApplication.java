912a8f31a2ba9d968d23e85194a44c18d09e1299
Cleiton <toniclecb@gmail.com>
2022-11-15 22:34:07 -0300

    First commit - project made by spring-initializr - no changes

---
+++ src/main/java/com/toniclecb/springbootregistration/SpringbootregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.toniclecb.springbootregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootregistrationApplication.class, args);
+	}
+
+}
